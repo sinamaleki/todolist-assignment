bbbe31ca64e89d8821842f046ab6297ce7e121e8
Sina <sina@Sinas-MacBook-Pro-2.local>
2024-09-03 12:30:39 -0700

    Fix deleteTodo function to correctly remove the specified todo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import Head from "next/head";
-import React, { useCallback, useState } from "react";
-import { Todo } from "@/types/todo";
+import React, {useCallback, useState} from "react";
+import {Todo} from "@/types/todo";
 import AddTodoForm from "@/components/AddTodoForm"
 import TodoList from "@/components/TodoList";
 import Banner from "@/components/Banner";
@@ -10,9 +10,9 @@ import sampleData from "@/sampleData.json";
  * Home: renders the To Do list page. Which is essentially a form component for creating To Dos and 3 todo lists
  * Each TodoList renders TodoItem components for each todo passed in
  * The 3 lists are for urgent, non-urgent, and completed
- * 
+ *
  * There are also several utility functions
- * 
+ *
  * AddTodo - create a new To Do
  * deleteTodo - delete a To Do via supplied id
  * toggleProperty - toggles isCompleted or isUrgent for supplied id
@@ -21,76 +21,82 @@ import sampleData from "@/sampleData.json";
  * displayComplete - calls displayTodoList with a filtered To Do selection
  */
 export default function Home() {
-  const [todos, setTodos] = useState<Todo[]>(sampleData);
+    const [todos, setTodos] = useState<Todo[]>(sampleData);
 
-  const AddTodo = (title: string, desc: string) => {
-    const newTodo: Todo = {
-      id: todos.length + 1,
-      title: title,
-      description: desc,
-      isCompleted: false,
-      isUrgent: false,
+    const AddTodo = (title: string, desc: string) => {
+        const newTodo: Todo = {
+            id: todos.length + 1,
+            title: title,
+            description: desc,
+            isCompleted: false,
+            isUrgent: false,
+        };
+
+        todos.push(newTodo);
+        setTodos(todos);
     };
 
-    todos.push(newTodo);
-    setTodos(todos);
-  };
+    const deleteTodo = (id: number) => {
+        // Incorrect logic: This line kept only the todo with the specified ID instead of deleting it
+        // setTodos(todos.filter((todo) => todo.id === id));
 
-  const deleteTodo = (id: number) => {
-    setTodos(todos.filter((todo) => todo.id === id));
-  };
+        // Correct logic: This line filters out the todo with the specified ID, effectively deleting it
+        // Edited by Sina on branch bugfix-3
+        setTodos(todos.filter((todo) => todo.id !== id));
 
-  const toggleProperty = useCallback((id: number, property: keyof Pick<Todo, 'isCompleted' | 'isUrgent'>) => {
-    const updatedTodos = todos.map((todo) => {
-      if (todo.id === id) {
-        todo[property] = !todo[property] as boolean;
-      }
-      return todo;
-    });
-    setTodos(updatedTodos);
-  }, [setTodos]);
+    };
 
-  const displayTodoList = (todoList:Todo[]) => {
+    const toggleProperty = useCallback((id: number, property: keyof Pick<Todo, 'isCompleted' | 'isUrgent'>) => {
+        const updatedTodos = todos.map((todo) => {
+            if (todo.id === id) {
+                todo[property] = !todo[property] as boolean;
+            }
+            return todo;
+        });
+        setTodos(updatedTodos);
+    }, [setTodos]);
+
+    const displayTodoList = (todoList: Todo[]) => {
+        return (
+            <TodoList
+                todos={todoList}
+                deleteTodo={deleteTodo}
+                toggleComplete={(id) => toggleProperty(id, 'isCompleted')}
+                toggleUrgent={(id) => toggleProperty(id, 'isUrgent')}
+            />
+        );
+    };
+
+    const displayTodos = (displayUrgent: boolean) => {
+        return displayTodoList(todos.filter((x) => {
+            if (displayUrgent) {
+                return !x.isCompleted && x.isUrgent === displayUrgent;
+            } else {
+                return !x.isCompleted && x.isUrgent !== displayUrgent;
+            }
+        }));
+    };
+
+    const displayComplete = () => {
+        return displayTodoList(todos.filter((x) => x.isCompleted));
+    };
+
     return (
-      <TodoList
-        todos={todoList}
-        deleteTodo={deleteTodo} 
-        toggleComplete={(id) => toggleProperty(id, 'isCompleted')} 
-        toggleUrgent={(id) => toggleProperty(id, 'isUrgent')} 
-      />
+        <>
+            <Head>
+                <title>To Do List</title>
+                <meta name="description" content="To Do List App"/>
+                <meta name="viewport" content="width=device-width, initial-scale=1"/>
+                <link rel="icon" href="favicon.ico"/>
+            </Head>
+
+            <div className="Home">
+                <Banner/>
+                <AddTodoForm addTodo={AddTodo}/>
+                {displayTodos(true)}
+                {displayTodos(false)}
+                {displayComplete()}
+            </div>
+        </>
     );
-  };
-
-  const displayTodos = (displayUrgent: boolean) => {
-    return displayTodoList(todos.filter((x) => {
-      if (displayUrgent) {
-        return !x.isCompleted && x.isUrgent === displayUrgent;
-      } else {
-        return !x.isCompleted && x.isUrgent !== displayUrgent;
-      }
-    }));
-  };
-
-  const displayComplete = () => {
-    return displayTodoList(todos.filter((x) => x.isCompleted));
-  };
-
-  return (
-    <>
-      <Head>
-        <title>To Do List</title>
-        <meta name="description" content="To Do List App" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="favicon.ico" />
-      </Head>
-
-      <div className="Home">
-        <Banner />
-        <AddTodoForm addTodo={AddTodo}/>
-        {displayTodos(true)}
-        {displayTodos(false)}
-        {displayComplete()}
-      </div>
-    </>
-  );
 }
